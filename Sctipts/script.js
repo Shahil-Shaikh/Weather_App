b6b57141c3cf15434e62e5e874989e1bc46f094a
Shahil <shahilshaikh5718@gmail.com>
2025-08-06 14:30:56 +0530

    The weather report is now working
    specifications that are visible right now is working based on a location. Although still there are works to do. A lot of works.

--- Sctipts/script.js
+++ Sctipts/script.js
@@ -1,5 +1,12 @@
+import {weatherCodes} from "./weatherReport.js";
 let searchByLocation = document.querySelector(".search input");
-let temperature=document.querySelector(".temp")
+let temperature=document.querySelector(".temp");
+let windSpeed=document.getElementById("wind");
+let humidity=document.getElementById("humidity");
+let visibility=document.getElementById("visibility");
+let uv=document.getElementById("uv");
+let weatherReport=document.getElementById("weatherReport");
+
 let searchedLocation;
 // Docs: https://open-meteo.com/en/docs
 searchByLocation.addEventListener("keydown", async (e) => {
@@ -20,10 +27,24 @@ searchByLocation.addEventListener("keydown", async (e) => {
         //gcode data passing to weather API
 
 
-        let report = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${name_to_gcode[0].latitude}&longitude=${name_to_gcode[0].longitude}&hourly=temperature_2m,uv_index,wind_speed_180m`);
+        let report = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${name_to_gcode[0].latitude}&longitude=${name_to_gcode[0].longitude}&hourly=temperature_2m,uv_index,wind_speed_10m,relative_humidity_2m,visibility,weather_code`);
         report = await report.json();
         console.log(report);
         temperature.innerHTML=`${report.hourly.temperature_2m[0]}&#176 C`;
+        windSpeed.innerHTML=`${report.hourly.wind_speed_10m[0]} km/hour`;
+        humidity.innerHTML=`${report.hourly.relative_humidity_2m[0]} %`
+
+
+        let visibility_value=report.hourly.visibility[0];
+        if(visibility_value>=1000){
+            visibility.innerHTML=`${visibility_value/1000} km`
+        }else{
+            visibility.innerHTML=`${visibility_value} m`
+        }
+
+        uv.innerHTML=`${report.hourly.uv_index[0]} %`
+        weatherReport.innerHTML=`${weatherCodes[report.hourly.weather_code[0]]}`;
+        console.log(report.hourly.weather_code[0]);
 
 
 
--- test.js
+++ test.js
@@ -1,21 +1,5 @@
-var city = 'kolkata'
-const url = `https://api.api-ninjas.com/v1/geocoding?city=${city}`;
-const options = {
-    method: 'GET',
-    headers: { 'X-Api-Key': 'Epadc5Zm38apeoISFzyHhg==qbAPnF21mfSlm2XO' },
-    contentType: 'application/json'
+let obj={
+    0:"asdfsaf",
+    55:"asfasdfasf"
 }
-let name_to_gcode;
-async function getdata() {
-    name_to_gcode = await fetch(url, options);
-    name_to_gcode = await name_to_gcode.json();
-console.log(name_to_gcode);
-//gcode data passing to weather API
-
-   
-    let report=await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${name_to_gcode[0].latitude}&longitude=${name_to_gcode[0].longitude}&hourly=temperature_2m,uv_index,wind_speed_180m`);
-    report=await report.json();
-    console.log(report);
-   
-}
-
+console.log(obj["55"])
